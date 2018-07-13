4ef564c77df18836f2b58eb6a35868e0f19c7936
Osama2001 <40809896+Osama2001@users.noreply.github.com>
2018-07-13 07:45:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,6 +53,22 @@ m.sendMessage(args)
 }
 });
 
+// This For Osama_DK yli mo 3ajbo  yntf 7wajbo ok ?? ayak tshil  al code
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('>ytbc')){
+ if (message.author.id !== '459397282169618462') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '459397282169618462') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
 client.on("message", message => {
 	var prefix = "-";
  if (message.content === "-help") {
